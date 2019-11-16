f2e791e9b25074f27507b24cc28947d4cdea2584
gemeshunor <47829017+gemeshunor@users.noreply.github.com>
2019-11-16 17:50:16 +0100

    idozites
    idozites kesz elmeletileg
    a megfelelo idopontban kiirja az aktualis idot es hogy honnan hova szeretnenk menni

--- src/main/java/bme/tmit/telki/controllers/TrafficWatchController.java
+++ src/main/java/bme/tmit/telki/controllers/TrafficWatchController.java
@@ -27,7 +27,17 @@ public class TrafficWatchController {
         model.addAttribute("name", "traffic");
 
         DistanceMatrixClient distanceMatrixClient = new DistanceMatrixClient();
-        distanceMatrixClient.sendApiRequest();
+        //distanceMatrixClient.sendApiRequest();
+        distanceMatrixClient.Telki_Budapest_22_00();
+        distanceMatrixClient.Telki_Budapest_00_04();
+        distanceMatrixClient.Telki_Budapest_04_06();
+        distanceMatrixClient.Telki_Budapest_06_12();
+        distanceMatrixClient.Telki_Budapest_12_22();
+
+        distanceMatrixClient.Budapest_Telki_00_06();
+        distanceMatrixClient.Budapest_Telki_06_14();
+        distanceMatrixClient.Budapest_Telki_14_20();
+        distanceMatrixClient.Budapest_Telki_20_24();
 
         return "main";
     }
--- src/main/java/bme/tmit/telki/distance_matrix/DistanceMatrixClient.java
+++ src/main/java/bme/tmit/telki/distance_matrix/DistanceMatrixClient.java
@@ -2,8 +2,14 @@ package bme.tmit.telki.distance_matrix;
 
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.model.*;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Date;
 
 /**
  * Komment:
@@ -41,9 +47,18 @@ import java.time.Instant;
  *
  *
  */
+
+
+@Component
 public class DistanceMatrixClient {
 
+    private static final Logger LOG = LoggerFactory.getLogger(DistanceMatrixClient.class);
+
+    /*
+    @Scheduled(cron = "0/5 * * * * ?")
     public void sendApiRequest() {
+
+
         System.out.println("starting request");
 
         final LatLng telki_center = new LatLng(47.5468889, 18.8283889);     // https://www.google.com/maps/@47.5475926,18.8278371,16.5z/data=!4m5!3m4!1s0x0:0x0!8m2!3d47.5468889!4d18.8283889
@@ -67,7 +82,75 @@ public class DistanceMatrixClient {
             }
         }
 
+        System.out.println("test");
+
+    }*/
+
+
+    @Scheduled(cron = "0 0/20 22-23 * * ?")
+    public void Telki_Budapest_22_00() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Telki_Budapest_22_00" + formatter.format(date));
     }
+
+    @Scheduled(cron = "0 0/20 0-3 * * ?")
+    public void Telki_Budapest_00_04() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Telki_Budapest_00_04" + formatter.format(date));
+    }
+
+    @Scheduled(cron = "0 0/10 4-5 * * ?")
+    public void Telki_Budapest_04_06() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Telki_Budapest_04_06: " + formatter.format(date));
+    }
+
+    @Scheduled(cron = "0 0/6 6-11 * * ?")
+    public void Telki_Budapest_06_12() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Telki_Budapest_06_12: " + formatter.format(date));
+    }
+
+    @Scheduled(cron = "0 0/10 12-21 * * ?")
+    public void Telki_Budapest_12_22() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Telki_Budapest_12_22: " + formatter.format(date));
+    }
+
+    @Scheduled(cron = "0 0/20 0-5 * * ?")
+    public void Budapest_Telki_00_06() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Budapest_Telki_00_06: " + formatter.format(date));
+    }
+
+    @Scheduled(cron = "0 0/10 6-13 * * ?")
+    public void Budapest_Telki_06_14() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Budapest_Telki_06_14: " + formatter.format(date));
+    }
+
+    @Scheduled(cron = "0 0/6 14-19 * * ?")
+    public void Budapest_Telki_14_20() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Budapest_Telki_14_20: " + formatter.format(date));
+    }
+
+    @Scheduled(cron = "0 0/10 20-23 * * ?")
+    public void Budapest_Telki_20_24() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Budapest_Telki_20_24: " + formatter.format(date));
+    }
+
+    //https://dzone.com/articles/running-on-time-with-springs-scheduled-tasks?fbclid=IwAR3HXEYVq4u_ftT3qzJX8oxa4GoO8lMlUUlbJA5vw40vaDHS2ruePbtYSwc
 
     // https://github.com/googlemaps/google-maps-services-java
     // https://developers.google.com/maps/documentation/distance-matrix/start
